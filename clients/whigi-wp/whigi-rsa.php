ba0bf7ff13d1593d30b53378330414ca8fd93970
Grégoire Mathonet <greg.mathonet@gmail.com>
2016-09-06 18:36:46 +0200

    Start implementing RSA

---
+++ clients/whigi-wp/whigi-rsa.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Whigi-RSA
+Plugin URI: http://envict.com
+Description: PHP RSA Implemntation of decryption, based on helpers listed in LICENSE. 
+Version: 0.1.1
+Author: Grégoire Mathonet
+Author URI: http://envict.com
+License: MIT
+*/
+
+include 'whigi-bn.php';
+
+
+
+?>
